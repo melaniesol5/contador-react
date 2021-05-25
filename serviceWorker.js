b0fdf96b4f6244b593ae25c57f0431633f560230
melaniesol5 <melaniealarcon97@hotmail.com>
2021-05-25 16:26:00 -0300

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,36 @@
+const CACHE_ELEMENTS= [
+    "./",
+    "https://unpkg.com/react@17/umd/react.production.min.js",
+    "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+    "https://unpkg.com/@babel/standalone/babel.min.js",
+    "./style.css",
+    "./components/Contador.js"
+];
+const CACHE_NAME = "v2_cache_name_react"
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            cache.addAll(CACHE_ELEMENTS).then(() => {
+                self.skipWaiting()
+            }).catch(console.log)
+        })
+    )
+});
+
+self.addEventListener("activate", (e) => {
+    const cacheWhiteList = [CACHE_NAME];
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(cacheNames.map(cacheName => {
+                return cacheWhiteList.indexOf(cacheName) === -1 && caches.delete(cacheName);  
+            }))
+        }).then(self.clients.claim())
+    )
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => res ? res : fetch(e.request)) 
+    )
+});
